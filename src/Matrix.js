283f11515b5c2170034de29d1291dd2aeafbd37c
Victoria <victoriathorsenn@gmail.com>
2021-07-06 17:30:35 -0400

    commit tree 756812cd95f6bcb7e70347448b5e322bd7e4b203

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 
+import Cell from './Cell.js'
+
 export default class Matrix extends Component {
   
   genRow = (vals) => {
-    return vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
+    // console.log('vals', vals)
+    return vals.map(val => <Cell value={val}/>) // replace me and render a cell component instead!
   }
   
   genMatrix = () => {
+    // console.log('gen', this.props.values)
     return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
   }
   
@@ -17,5 +21,21 @@ export default class Matrix extends Component {
       </div>
     )
   }
+
   
+}
+
+Matrix.defaultProps ={
+  values: [
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+  ]
 }
--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@ import './index.css';
 import { pattern1 } from './data.js'
 import Matrix from './Matrix.js'
 
-ReactDOM.render(<Matrix values={pattern1} />, document.getElementById('root'));
+ReactDOM.render(
+    <Matrix values={pattern1} />, document.getElementById('root')
+    );
